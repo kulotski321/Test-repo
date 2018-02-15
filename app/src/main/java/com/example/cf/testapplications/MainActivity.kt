481109c7dd29614570b18e67e25f1799dc037c75
Angelo <angelocool177@gmail.com>
2018-02-15 23:41:26 +0800

    Added some random functions on MainActivity.kt

--- app/src/main/java/com/example/cf/testapplications/MainActivity.kt
+++ app/src/main/java/com/example/cf/testapplications/MainActivity.kt
@@ -10,6 +10,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
-    //This is just for testing
-    //2nd commit
+    fun printSum(a: Int, b:Int){
+        println("sum of $a and $b is ${a+b}")
+    }
+    fun main(args: Array<String>){
+        printSum(-1,8)
+
+        val num1: Int = 1
+        val num2 =2
+        val num3: Int
+        num3 = 3
+
+        println(" $num1, $num2, and $num3")
+
+        var x = 5
+        x += 1
+    }
+    fun stringTemplates(){
+        var a = 1
+        val s1 = "a is $a"
+
+        a = 2
+        val s2 = "${(s1.replace("is","was"))},but now is $a"
+    }
+
 }
